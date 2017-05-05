1f1555c377b496c8aafa311af15a5f786204eba8
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-05-06 00:25:27 +0200

    Update create_all.sh

--- build-scripts/homeassistant/create_all.sh
+++ build-scripts/homeassistant/create_all.sh
@@ -3,6 +3,6 @@ set -e
 
 for machine in "raspberrypi3" "raspberrypi2" "raspberrypi" "qemux86" "qemux86-64"
 do
-    ./create_homeassistant.sh -m $machine "@$" &
+    ./create_homeassistant.sh -m $machine "$@" &
 done
 wait
