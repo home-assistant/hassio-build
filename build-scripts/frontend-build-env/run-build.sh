6e96cb0b9c0c5f51f32735801ade04de9010606a
Pascal Vizeli <pvizeli@syshack.ch>
2017-05-14 01:31:40 +0200

    Update scripts

--- build-scripts/frontend-build-env/run-build.sh
+++ build-scripts/frontend-build-env/run-build.sh
@@ -2,6 +2,7 @@
 set -e
 
 # build frontend
+cd home-assistant-polymer
 yarn
 ./node_modules/.bin/bower install --allow-root
 yarn run frontend_prod
@@ -11,5 +12,5 @@ cd build-temp/
 gzip -f -k -9 ./*.html
 
 # move to frontend
-cp -f hassio-main.html ../hassio/panel/
-cp -f hassio-main.html.gz ../hassio/panel/
+cp -f hassio-main.html ../../hassio/panel/
+cp -f hassio-main.html.gz ../../hassio/panel/
