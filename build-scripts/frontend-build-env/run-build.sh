0f9f70308139498801bdbcb8b56cf3f4e2c30094
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-05-14 00:04:27 +0200

    Add frontend build env

---
+++ build-scripts/frontend-build-env/run-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# build frontend
+yarn
+./node_modules/.bin/bower install --allow-root
+yarn run frontend_prod
+
+# prepare data
+cd build-temp/
+gzip -f -k -9 ./*.html
+
+# move to frontend
+cp -f hassio-main.html ../hassio/panel/
+cp -f hassio-main.html.gz ../hassio/panel/
