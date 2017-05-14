1ec3a8dac8d0b992b9b67199bec7f17a2101ad62
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-05-14 18:42:40 +0200

    add cache to oder build

--- build-scripts/hassio-supervisor/create_hassio_supervisor.sh
+++ build-scripts/hassio-supervisor/create_hassio_supervisor.sh
@@ -3,6 +3,7 @@ set -e
 
 BUILD_CONTAINER_NAME=hassio-build-$$
 DOCKER_PUSH="false"
+DOCKER_CACHE="true"
 DOCKER_HUB=homeassistant
 BRANCH=master
 REPOSITORY=https://github.com/home-assistant/hassio
@@ -44,6 +45,8 @@ Options:
         Version/Tag of Hass.IO build.
     -p, --push
         Upload the build to docker hub.
+    -n, --no-cache
+        Disable build from cache
 EOF
 }
 
@@ -77,6 +80,9 @@ while [[ $# -gt 0 ]]; do
             ;;
         -p|--push)
             DOCKER_PUSH="true"
+            ;;
+        -n|--no-cache)
+            DOCKER_CACHE="false"
             ;;
         *)
             echo "[WARNING] $0 : Argument '$1' unknown. Ignoring."
@@ -127,6 +133,7 @@ docker run --rm \
     -v "$WORKSPACE":/docker \
     -v ~/.docker:/root/.docker \
     -e DOCKER_PUSH=$DOCKER_PUSH \
+    -e DOCKER_CACHE=$DOCKER_CACHE \
     -e DOCKER_IMAGE="$DOCKER_IMAGE" \
     -e DOCKER_TAG="$DOCKER_TAG" \
     --name $BUILD_CONTAINER_NAME \
