b76bf0c7f6c22b03fd10775c62ccc3ab525ccdf5
sytone <github@sytone.com>
2017-06-07 20:10:47 -0700

    Refined approach to install and service Used a powershell script for service creation that I have used in the past to make it simpler and will be modifying it to be the control process as well.

--- install/hassio_install.ps1
+++ install/hassio_install.ps1
@@ -1,3 +1,6 @@
+#requires -version 4.0
+#requires -runasadministrator
+
 param (
     [string]
     $DataSharePath = "f:\docker\hassio",
@@ -10,36 +13,37 @@ param (
 
     [string]
     $HassIoSourceBranch = "master"
-
 )
-# Setup Variables
-$DOCKER_REPO="homeassistant"
-$ARCH="amd64"
-$URL_VERSION="https://raw.githubusercontent.com/home-assistant/hassio/master/version.json"
-$URL_START_HASSIO="$HassIoSourceUrlRoot/$HassIoSourceBranch/install/misc/hassio-start"
-$URL_GENERIC_HC="$HassIoSourceUrlRoot/$HassIoSourceBranch/generic-hc"
-$URL_SERVICE_HASSIO="$HassIoSourceUrlRoot/$HassIoSourceBranch/install/systemd/hassio-supervisor.service"
-$URL_SERVICE_HOSTCONTROLL="$HassIoSourceUrlRoot/$HassIoSourceBranch/install/systemd/hassio-hc.service"
-$MACHINE=$ARCH
-$HOMEASSISTANT_DOCKER="$DOCKER_REPO/$MACHINE-homeassistant"
-$HASSIO_DOCKER="$DOCKER_REPO/i386-hassio-supervisor"
 
+# Setup Variables
+$URL_VERSION="https://raw.githubusercontent.com/home-assistant/hassio/master/version.json"
+$URL_HASSIO_SERVICE="$HassIoSourceUrlRoot/$HassIoSourceBranch/install/service/hassio_supervisor.ps1"
+
+$DOCKER_REPO="homeassistant"
+$HOMEASSISTANT_DOCKER="$DOCKER_REPO/qemux86-64-homeassistant"
+$HASSIO_DOCKER="$DOCKER_REPO/amd64-hassio-supervisor"
 
 if(!(Test-Path -Path $DataSharePath)) {
-    New-Item -Path $DataSharePath -ItemType Directory
+    New-Item -Path $DataSharePath -ItemType Directory | Out-Null
 }
 
 if(!(Test-Path -Path $BinaryLocation)) {
-    New-Item -Path $BinaryLocation -ItemType Directory
+    New-Item -Path $BinaryLocation -ItemType Directory | Out-Null
 }
+
+Write-Host "`n------------------------------------------" -ForegroundColor White
+Write-Host "- HASS.IO Installer for Windows          -" -ForegroundColor White
+Write-Host "-  This will install the hass.io system  -" -ForegroundColor White
+Write-Host "-  on docker in a windows environemnt.   -" -ForegroundColor White
+Write-Host "------------------------------------------" -ForegroundColor White
 
 # Read infos from web
 $HASSIO_VERSION= (ConvertFrom-Json  (Invoke-WebRequest -Uri $URL_VERSION).Content).hassio
 $GENERIC_HC_VERSION= (ConvertFrom-Json  (Invoke-WebRequest -Uri $URL_VERSION).Content).generic
 
+Write-Host "`n[INFO] Version Information"
 Write-Host "HASSIO_VERSION = $HASSIO_VERSION"
 Write-Host "GENERIC_HC_VERSION = $GENERIC_HC_VERSION"
-
 
 ##
 # Install supervisor
@@ -48,18 +52,27 @@ docker pull "$($HASSIO_DOCKER):$($HASSIO_VERSION)"
 docker tag "$($HASSIO_DOCKER):$($HASSIO_VERSION)" "$($HASSIO_DOCKER):latest" 
 
 ##
-# install generic hostcontrol
-Write-Host "[INFO] Install generic HostControl"
-(Invoke-WebRequest -Uri "$URL_GENERIC_HC/$GENERIC_HC_VERSION.ps1").Content | Set-Content -Path "$BinaryLocation/hassio-hc.ps1" -Encoding UTF8
+# Download service script
+Write-Host "[INFO] Downloading service script"
+(Invoke-WebRequest -Uri "$URL_HASSIO_SERVICE").Content | Set-Content -Path "$BinaryLocation\hassio_supervisor.ps1" -Encoding UTF8
 
 ##
-# install start-hassio
-Write-Host "[INFO] Install startup scripts"
-(Invoke-WebRequest -Uri "$URL_START_HASSIO.ps1").Content | Set-Content -Path "$BinaryLocation/hassio-start.ps1" -Encoding UTF8
+# Generate runtime configuration
+Write-Host "[INFO] Generating configuration: $(Join-Path $hassIoConfiguration 'configuration.json')"
+$defaultConfiguration = "{
+    'datapath':'$DataSharePath', 
+    'home_assistant_docker':'$HOMEASSISTANT_DOCKER',
+    'hassio_supervisor_docker':'$HASSIO_DOCKER'
+}"
+Write-Host "[INFO] ... $defaultConfiguration "
+$defaultConfiguration | Set-Content -Path (Join-Path $hassIoConfiguration 'configuration.json') -Encoding UTF8
 
+##
+# Installing Service
+Write-Host "[INFO] Installing service."
+& $BinaryLocation\hassio_supervisor.ps1 -Setup
 
-sed -i "s|%%SUPERVISOR_IMAGE%%|$HASSIO_DOCKER|g" "$BIN_DIR/hassio-start"
-sed -i "s|%%HOMEASSISTANT_IMAGE%%|$HOMEASSISTANT_DOCKER|g" "$BIN_DIR/hassio-start"
-sed -i "s|%%DATA_SHARE%%|$DATA_SHARE|g" "$BIN_DIR/hassio-start"
+##
+# Starting Service
+Write-Host "[INFO] Starting hass.io Service"
 
-chmod a+x "$BIN_DIR/hassio-start"
