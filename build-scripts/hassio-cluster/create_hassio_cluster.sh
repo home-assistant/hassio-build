a6b9d391186ae47df2cfa1eb81e1591ade992a91
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-07-27 11:32:14 +0200

    Update create_hassio_cluster.sh

--- build-scripts/hassio-cluster/create_hassio_cluster.sh
+++ build-scripts/hassio-cluster/create_hassio_cluster.sh
@@ -2,7 +2,7 @@
 set -e
 
 BUILD_CONTAINER_NAME=hassio-cluster-build-$$
-DOCKER_PUSH="false"
+DOCKER_PUSH="true"
 DOCKER_CACHE="true"
 DOCKER_WITH_LATEST="true"
 DOCKER_HUB=homeassistant
@@ -38,8 +38,8 @@ Options:
 
     -a, --arch armhf|aarch64|i386|amd64
         Arch for Hass.IO build.
-    -p, --push
-        Upload the build to docker hub.
+    -t, --test
+        Don't upload the build to docker hub.
     -n, --no-cache
         Disable build from cache
 EOF
@@ -65,8 +65,8 @@ while [[ $# -gt 0 ]]; do
             ARCH=$2
             shift
             ;;
-        -p|--push)
-            DOCKER_PUSH="true"
+        -t|--test)
+            DOCKER_PUSH="false"
             ;;
         -n|--no-cache)
             DOCKER_CACHE="false"
