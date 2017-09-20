83dc4e0eae2dc35917435be9b2deace48557abad
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-09-20 17:35:12 +0200

    Update run.sh

--- build-scripts/build-env/run.sh
+++ build-scripts/build-env/run.sh
@@ -6,17 +6,40 @@ set -e
 
 function print_help() {
     cat << EOF
-Hass.io build-env for add-ons:
+Hass.io build-env for ecosystem:
 docker run --rm homeassistant/build-env:latest [options]
 
 Options:
   -h, --help
         Display this help and exit.
   
-  -r, --repository
-        Set git repository to load addon from.
-  -b, --branch
-        Set git branch to load addon from.
+  Repository / Data handling
+    -r, --repository <REPOSITORY>
+        Set git repository to load data from.
+    -b, --branch <BRANCH>
+        Set git branch for repository.
+    -t, --target <PATH_TO_BUILD>
+        Set local folder or path inside repository for build.
+        
+  Architecture
+    --armhf
+        Build for arm.
+    --amd64
+        Build for intel/amd 64bit.
+    --aarch64
+        Build for arm 64bit.
+    --i386
+        Build for intel/amd 32bit.
+    --all
+        Build all architecture.
+
+  Build handling
+    -p, --push <HUB_REPOSITORY>
+       Set repository for dockerhub.
+    -l, --tag-latest
+       Tag images as latest.
+    -c, --cache
+       Enable cache for the build (from latest).
 EOF
 
     exit 1
