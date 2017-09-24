10b68867effe55ea28d36450b2a0429f0ab95ea3
Pascal Vizeli <pvizeli@syshack.ch>
2017-09-24 19:19:25 +0200

    Fix bug

--- builder/create.sh
+++ builder/create.sh
@@ -6,7 +6,7 @@ TAG="$(date +%Y%m%d)"
 # Create amd64-builder
 docker build -t "homeassistant/amd64-builder:$TAG" \
     --build-arg "BUILD_FROM=multiarch/ubuntu-core:amd64-xenial" \
-    --build-arg "amd64" \
+    --build-arg "BUILD_ARCH=amd64" \
     --label "io.hass.version=$TAG" \
     --label "io.hass.type=builder" \
     --label "io.hass.arch=amd64" \
@@ -17,7 +17,7 @@ docker tag "homeassistant/amd64-builder:$TAG" "homeassistant/amd64-builder:lates
 # Create armhf-builder
 docker build -t "homeassistant/armhf-builder:$TAG" \
     --build-arg "BUILD_FROM=multiarch/ubuntu-core:armhf-xenial" \
-    --build-arg "armhf" \
+    --build-arg "BUILD_ARCH=armhf" \
     --label "io.hass.version=$TAG" \
     --label "io.hass.type=builder" \
     --label "io.hass.arch=armhf" \
