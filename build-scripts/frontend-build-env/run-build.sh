74fda4d4b4d91f7cff1036c6f626dfaa35ee6550
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-11-14 22:34:12 -0800

    Build 2 panels

--- build-scripts/frontend-build-env/run-build.sh
+++ build-scripts/frontend-build-env/run-build.sh
@@ -13,12 +13,12 @@ nvm use
 yarn
 ./node_modules/.bin/bower install --allow-root
 npm install -g gulp-cli
-gulp hassio-panel
+gulp hassio-panel hassio-panel-es5
 
 # prepare data
 cd build-temp/
 gzip -f -k -9 ./*.html
 
 # move to frontend
-cp -f hassio-main.html ../../hassio/panel/
-cp -f hassio-main.html.gz ../../hassio/panel/
+cp -f hassio-main-*.html ../../hassio/panel/
+cp -f hassio-main-*.html.gz ../../hassio/panel/
