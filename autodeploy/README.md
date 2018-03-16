dca9aa10fd9dc116e9fd9378b974b925f1a954b6
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-03-16 20:06:01 +0100

    Autodeploy (#62)
    * Create README.md
    
    * Add script for auto deploy
    
    * Update ad_homeassistant.py
    
    * Update ad_homeassistant.py
    
    * Update ad_homeassistant.py
    
    * Update ad_homeassistant.py
    
    * Update ad_homeassistant.py
    
    * Update ad_homeassistant.py
    
    * make it old python compatible
    
    * fix join
    
    * fix path
    
    * write logfile

---
+++ autodeploy/README.md
@@ -0,0 +1,9 @@
+# AutoDeploy
+
+This python script build Home-Assistant container automatic and monitor repositories.
+
+## Requrements
+
+```bash
+$ pip3 install requests
+```
---
+++ autodeploy/ad_homeassistant.py
@@ -0,0 +1,110 @@
+"""Automatic deploy Home-Assistant container."""
+import argparse
+import logging
+import subprocess
+import time
+
+import requests
+
+RELEASE_URL = \
+    'https://api.github.com/repos/home-assistant/home-assistant/releases'
+
+
+def parse_args():
+    """Parse program arguments."""
+    parser = argparse.ArgumentParser(
+        description="Automat deploying of Home-Assistant containers."
+    )
+    parser.add_argument(
+        "--builder-arch", dest='builder', required=True,
+        help="Builder arch to use for builds")
+    parser.add_argument(
+        "--latest-build", dest='version', required=True,
+        help="Version of latest build")
+    parser.add_argument(
+        "--arch", action='append', dest='architectures', required=True,
+        help="Run build for this architecture")
+    parser.add_argument(
+        "--machines", dest='machines', required=True,
+        help="Run machine build for this")
+
+    return parser.parse_args()
+
+
+def get_releases(until=None):
+    """Read releases into list."""
+    try:
+        release_data = requests.get(RELEASE_URL).json()
+    except requests.exceptions.RequestException:
+        logging.exception("Can't read releases")
+        release_data = []
+
+    for row in release_data:
+        tag = row['tag_name']
+        if tag == until:
+            break
+        yield tag
+
+
+def run_build(builder, architectures, machines, version):
+    """Run Build."""
+    generic = ("docker run --rm --privileged -v ~/.docker:/root/.docker "
+               "-v /var/run/docker.sock:/var/run/docker.sock "
+               "homeassistant/{}-builder "
+               "-r https://github.com/home-assistant/hassio-build "
+               "-t homeassistant/generic --docker-hub homeassistant "
+               "--{} --homeassistant {}").format(
+                   builder, " --".join(architectures), version)
+
+    machine = ("docker run --rm --privileged -v ~/.docker:/root/.docker "
+               "-v /var/run/docker.sock:/var/run/docker.sock "
+               "homeassistant/{}-builder "
+               "-r https://github.com/home-assistant/hassio-build "
+               "-t homeassistant/machine --docker-hub homeassistant "
+               "--homeassistant-machine {}={}").format(
+                   builder, version, machines)
+
+    logging.info("Start generic build of %s", version)
+    run_generic = subprocess.run(
+        generic, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Write log
+    with open("generic-{}.log".format(version), "w") as logfile:
+        logfile.write("{}\n{}".format(run_generic.stdout, run_generic.stderr))
+    run_generic.check_returncode()
+
+    logging.info("Start generic machine of %s", version)
+    run_machine = subprocess.run(
+        machine, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Write log
+    with open("machine-{}.log".format(version), "w") as logfile:
+        logfile.write("{}\n{}".format(run_machine.stdout, run_machine.stderr))
+    run_machine.check_returncode()
+
+
+def main():
+    """Run automatic build system."""
+    args = parse_args()
+
+    latest_build = args.version
+    while True:
+        for release in get_releases(latest_build):
+            run_build(args.builder, args.architectures, args.machines, release)
+            latest_build = release
+            logging.info("Build of release %s done", release)
+
+        # Wait 10 min before
+        time.sleep(600)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
+    try:
+        logging.info("Start Build System")
+        main()
+    except Exception:  # pylint: disable=W0703
+        logging.exception("Fatal Error on Build System")
+    else:
+        logging.info("Stop Build System")
