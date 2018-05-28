8d2d154b0d10d4ccc8c107d77339a5e2cb018fd6
Paulus Schoutsen <balloob@gmail.com>
2018-05-28 09:14:28 -0400

    Update run-build

--- frontend-build/run-build.sh
+++ frontend-build/run-build.sh
@@ -5,16 +5,14 @@ set -e
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+cd home-assistant-polymer
+nvm install
+script/bootstrap
 
 # build frontend
-cd home-assistant-polymer/hassio/script
-./build_hassio
+cd hassio
+./script/build_hassio
+gzip -f -k -9 build-es5/*
 
-# prepare data
-mkdir -p ../hassio/api/panel
-
-cp -f build-hassio/index.html ../hassio/api/panel/index.html
-gzip -f -k -9 ../hassio/api/panel/index.html
-
-cp -f build-hassio/hassio-app.html ../hassio/api/panel/hassio-app.html
-gzip -f -k -9 ../hassio/api/panel/hassio-app.html
+# Copy frontend
+cp -rf build-es5/* ../../hassio/api/panel/
