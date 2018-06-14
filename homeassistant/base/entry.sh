77cd115f76c566fae271e89c3f6f69fb02444656
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-06-14 17:05:54 +0200

    Udev (#86)
    * Create entry.sh
    
    * Rename homeassistant/generic/entry.sh to homeassistant/base/entry.sh
    
    * Update Dockerfile

---
+++ homeassistant/base/entry.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+udevd --daemon
+udevadm trigger &> /dev/null
+	
+if CMD="$(which "$1")"; then
+  shift
+  exec "$CMD" "$@"
+else
+  echo "Command not found: $1"
+  exit 1
+fi
