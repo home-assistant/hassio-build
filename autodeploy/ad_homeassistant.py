c40ae43c4624cda3342bbe5cd536b92d96b0873e
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-12-11 16:07:20 +0100

    Update ad_homeassistant.py

--- autodeploy/ad_homeassistant.py
+++ autodeploy/ad_homeassistant.py
@@ -41,8 +41,10 @@ def get_releases(until=None):
         logging.exception("Can't read releases")
         release_data = []
 
-    release_data.sort(key=StrictVersion, reverse=True)
-    for row in release_data:
+    release_list = [row['tag_name'] in release_data]
+    release_list.sort(key=StrictVersion, reverse=True)
+
+    for row in release_list:
         tag = row['tag_name']
         if tag == until:
             break
